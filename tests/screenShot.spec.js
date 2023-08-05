31f17ea8e58f08fabd82b237c5b4827d69059ed8
radhanapa <radha.napa@gmail.com>
2023-08-04 21:57:33 -0500

    removed only tag

--- tests/screenShot.spec.js
+++ tests/screenShot.spec.js
@@ -12,7 +12,7 @@ test('Ful page screenshot', async ({page})=>{
 
 })
 
-test.only('element screenshot', async ({page})=>{
+test('element screenshot', async ({page})=>{
  await page.goto('https://demo.opencart.com/')
  await page.locator('//*[@id="content"]/div[2]/div[1]/form/div/div[2]').screenshot({path:'tests/screenshots/'+Date.now()+'element.png'})
 })
